a6ebe856ee02e2c2af02749b17fdf3c850c187bb
pauldhalluin <paul.dhalluin@mines-paristech.fr>
2023-01-26 17:04:47 +0100

    test

--- model.py
+++ model.py
@@ -148,4 +148,5 @@ class GatedAttention(nn.Module):
         Y_prob, _, A = self.forward(X)
         Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
         neg_log_likelihood = -1. * (Y * torch.log(Y_prob).squeeze() + (1. - Y) * torch.log(1. - Y_prob).squeeze()) # negative log bernoulli
+        print(neg_log_likelihood)
         return neg_log_likelihood, A
