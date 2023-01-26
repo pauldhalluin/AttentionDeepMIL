7867677979e6e4ef5d9b3ba02f2634d086c9a1e9
pauldhalluin <paul.dhalluin@mines-paristech.fr>
2023-01-26 16:12:38 +0100

    dim fix

--- model.py
+++ model.py
@@ -128,7 +128,8 @@ class GatedAttention(nn.Module):
         A = self.attention_weights(A_V * A_U) # element wise multiplication # NxK
         print(A.size())
         A = torch.transpose(A, 2, 1)  # KxN
-        A = F.softmax(A, dim=1)  # softmax over N
+        # A = torch.transpose(A, 1, 0)  # KxN
+        A = F.softmax(A, dim=2)  # softmax over N
         print(A.size())
 
         M = torch.mm(A, H)  # KxL
