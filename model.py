e56675627fbb1ccfc14c5b4d71b11fe1df00732b
pauldhalluin <paul.dhalluin@mines-paristech.fr>
2023-01-26 16:52:23 +0100

    print loss

--- model.py
+++ model.py
@@ -147,6 +147,7 @@ class GatedAttention(nn.Module):
         Y = Y.float()
         Y_prob, _, A = self.forward(X)
         Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
-        neg_log_likelihood = torch.mean(-1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob)), axis=0) # negative log bernoulli
+        neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob)) # negative log bernoulli
         print(neg_log_likelihood)
+        print(neg_log_likelihood.size())
         return neg_log_likelihood, A
